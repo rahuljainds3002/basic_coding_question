9851e07570c9106110f872c5d91a62054ac58e0a
rahul jain <rahul.jainds3002@gmail.com>
2024-06-30 22:27:59 +0530

    pattern_5

---
+++ src/main/scala/pattern_5.scala
@@ -0,0 +1,13 @@
+object pattern_5 {
+  def main(args: Array[String]): Unit = {
+    for(i<-1 to 5 ){
+      print("*")
+      for(j<-1 to i){
+        print("_*")
+
+        }
+      println()
+      }
+
+    }
+  }
