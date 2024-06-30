c1d24d7f5782f503f4ef8f76f346556123ed1d70
rahul jain <rahul.jainds3002@gmail.com>
2024-06-30 21:20:07 +0530

    question26

---
+++ src/main/scala/question26.scala
@@ -0,0 +1,10 @@
+object question26 {
+  def main(args: Array[String]): Unit = {
+    for(i<-1 to 36 ){
+      var a = i%2
+      if(a==0){print("even,")}
+      else { print(s" $i,")}
+    }
+  }
+
+}
