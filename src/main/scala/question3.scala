d2c5dfd7e7fbd547dc3514368bdbb31a91e93faf
rahul jain <rahul.jainds3002@gmail.com>
2024-06-30 20:10:11 +0530

    question3

---
+++ src/main/scala/question3.scala
@@ -0,0 +1,13 @@
+object question3 {
+  def main(args: Array[String]): Unit ={
+    var a  = -10
+    var b = -200
+    var c = -30
+
+    if(a>b&&a>c){ print( s" variable a $a is greatest")}
+    else if (b>a&&b>c){ print( s" variable b $b is greatest")}
+    else { print( s" variable c $c is greatest")}
+
+  }
+
+}
