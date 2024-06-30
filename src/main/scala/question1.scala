e34bd02c1f400284ac55328e3e5dd2a7e4ff5da7
rahul jain <rahul.jainds3002@gmail.com>
2024-06-30 20:04:09 +0530

    question1

--- src/main/scala/question1.scala
+++ src/main/scala/question1.scala
@@ -1,4 +1,9 @@
 object question1 {
-  abc
+  def main(args: Array[String]): Unit ={
 
+    var weight  = 16
+    var weight_in_gram = weight * 1000
+    print(s"weigh $weight kg is $weight_in_gram gms")
+
+  }
 }
