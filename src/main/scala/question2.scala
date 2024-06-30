382e12d89cfba6065f3c9ebe8900dee27631b37f
rahul jain <rahul.jainds3002@gmail.com>
2024-06-30 20:06:32 +0530

    question2

---
+++ src/main/scala/question2.scala
@@ -0,0 +1,11 @@
+object question2 {
+  def main(args: Array[String]): Unit ={
+
+    var temp  = 16
+    var temp_in_faren = (temp * 9 / 5) + 32
+    print(s"temp $temp deg celcius is $temp_in_faren F")
+
+  }
+
+
+}
